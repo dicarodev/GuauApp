8ba8128ce72e3116a1b4f8001b205e9944022d16
Cristiian17 <muresancristian.cm@gmail.com>
2024-01-31 18:40:29 +0100

    spinner added for breeds, province, cities

---
+++ app/src/main/java/com/guauapp/model/Breed.java
@@ -0,0 +1,21 @@
+package com.guauapp.model;
+
+public class Breed {
+    private String name;
+
+    public Breed() {
+    }
+
+    public Breed(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- app/src/main/java/com/guauapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/guauapp/ui/profile/ProfileFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,20 +18,28 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.guauapp.MainActivity;
 import com.guauapp.databinding.FragmentProfileBinding;
+import com.guauapp.model.Breed;
 import com.guauapp.model.Dog;
+import com.guauapp.model.DogsDAO;
+import com.guauapp.model.Province;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
     private FragmentProfileBinding binding;
+    private List<Province> provinceList;
+    private DogsDAO dogsDAO;
 
     DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
@@ -44,27 +55,80 @@ public class ProfileFragment extends Fragment {
         profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
-
     @Override
     public void onStart() {
         super.onStart();
+        dogsDAO = new DogsDAO();
         Button addDogBtn = binding.btnAddDog;
         addDogBtn.setOnClickListener(this::onClick);
+        setProvinces();
+        setBreeds();
+    }
+
+    private void setBreeds() {
+        dogsDAO.getBreedsAsync().thenAccept(breedList -> {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_activated_1, breedList);
+
+            binding.spnBreed.setAdapter(adapter);
+        }).exceptionally(exception -> {
+            return null;
+        });
+    }
+
+
+    public List<String> getBreeds() {
+        List<String> breedList = new ArrayList<>();
+        mDatabase.child("breed").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot productSnapshot : snapshot.getChildren()) {
+                    Breed breed = productSnapshot.getValue(Breed.class);
+                    breedList.add(breed.getName());
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, breedList);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                binding.spnBreed.setAdapter(adapter);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+        return breedList;
+    }
+
+
+
+    private void setProvinces() {
+        provinceList = dogsDAO.getProvinces();
+
+        binding.spnProvince.setAdapter(new ArrayAdapter<>(this.getContext(), android.R.layout.simple_list_item_activated_1, provinceList));
+        binding.spnProvince.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Province province = (Province) binding.spnProvince.getSelectedItem();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, province.getCities());
+        binding.spnCities.setAdapter(adapter);
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
     private void onClick(View view) {
         EditText dogName =  binding.txvDogName;
-        EditText breed =    binding.txvBreed;
+        String breed = binding.spnBreed.getSelectedItem().toString();
         EditText descrption =    binding.txvDescrption;
         EditText owenerName =    binding.txvOwenerName;
 
-        String province = "ciudad Real";
-        String location = "Alcazar";
+        String province = binding.spnProvince.getSelectedItem().toString();
+        String location = binding.spnCities.getSelectedItem().toString();
         ArrayList tags = new ArrayList();
         Dog dog = new Dog(4321,
                             dogName.getText().toString(),
                             owenerName.getText().toString(),
-                            breed.getText().toString(),
+                            breed,
                             province,
                             location,
                             tags,
@@ -100,5 +164,7 @@ public class ProfileFragment extends Fragment {
                     }
                 });
     }
+
+
 
 }
