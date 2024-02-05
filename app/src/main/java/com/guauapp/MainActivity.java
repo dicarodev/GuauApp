66f423fa9089da8be68ca130061c215785db2026
dicarodev <diego.carramolino06@gmail.com>
2024-02-05 13:27:38 +0100

    init config ChatFragment

--- app/src/main/java/com/guauapp/MainActivity.java
+++ app/src/main/java/com/guauapp/MainActivity.java
@@ -61,9 +61,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
 
         navView.setItemIconTintList(null);
-
-        navView.setVisibility(View.INVISIBLE);
-
     }
 
 
